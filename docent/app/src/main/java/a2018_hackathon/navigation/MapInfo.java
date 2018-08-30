f7fef39e53819237d12966c8bebfe6a53682e096
SeyoungJeon <34870901+SeyoungJeon@users.noreply.github.com>
2018-08-30 19:06:01 +0900

    feat: Node 클래스, MapInfo 클래스 생성

---
+++ docent/app/src/main/java/a2018_hackathon/navigation/MapInfo.java
@@ -0,0 +1,17 @@
+package a2018_hackathon.navigation;
+
+//맵 정보에 대한 상수 클래스
+public class MapInfo {
+
+    //벽
+    public static final int block = 0;
+
+    public static final int work_1 = 1;
+    public static final int work_2 = 2;
+    public static final int work_3 = 3;
+    public static final int work_4 = 4;
+    public static final int work_5 = 5;
+
+    public static int[][] museum = new int[][]{                                                                                                                                                                                      //
+    };
+}
